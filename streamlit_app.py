4f6305985b30da51f295f3a9f36091f9d0a11c08
freddiethefirestone <118886682+freddiethefirestone@users.noreply.github.com>
2023-03-24 11:27:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 
+
+streamlit.title('My Parents new Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
