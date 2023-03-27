34c79e60d5c21bdbd08b679d8810a3b7dfff704f
freddiethefirestone <118886682+freddiethefirestone@users.noreply.github.com>
2023-03-27 12:06:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,7 +29,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
@@ -35,8 +38,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
-import snowflake.connector
+# dont run anything past here while troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
